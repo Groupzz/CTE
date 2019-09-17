f6dc2f08d9b757d538d77522f3b62b8927f5c617
josh <joshlorenzen49@gmail.com>
2019-09-17 10:59:44 -0700

    Initial setup of stdin

---
+++ src/Lexer.java
@@ -0,0 +1,21 @@
+/* CECS 444 Compiler Construction
+ * Project 1: Lexer
+ * Authors: Aleks Dziewulska, Jamil Khan, Jessica Hilario, Josh Lorenzen
+ */
+
+import java.io.IOException;
+
+public class Lexer {
+
+
+	public static void main(String[] args) throws IOException {
+
+		int c;
+		int j = 1;
+		while ((c = System.in.read()) != '\n') {
+			System.out.println((char) c);
+		}
+	}
+
+
+}
