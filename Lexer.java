f6d19d8d3d60aa42b4821b413f84ce7ab961eb62
Aleks <jokerka17@gmail.com>
2019-09-17 10:26:26 -0700

    Create Lexer.java

---
+++ Lexer.java
@@ -0,0 +1,12 @@
+/* CECS 444 Compiler Construction
+ * Project 1: Lexer
+ * Authors: Aleks Dziewulska, Jamil Khan, Jessica Hilario, Josh Lorenzen
+ */
+
+class Lexer()
+{
+	public static void main(String args[])
+	{
+
+	}
+}
