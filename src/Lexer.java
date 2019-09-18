589de1d3b625e0149647bb556bee878f423a319f
Aleks <jokerka17@gmail.com>
2019-09-17 20:48:28 -0700

    Add keyword HashMap

--- src/Lexer.java
+++ src/Lexer.java
@@ -4,11 +4,14 @@
  */
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Lexer {
 
+	static HashMap<String, Integer> keywords;
 	public static void main(String[] args) throws IOException {
 
+		keywordInitialize();
 		int ci;
 		int line = 1;
 		int pos = 0;
@@ -27,5 +30,33 @@ public class Lexer {
 		}
 	}
 
+	static void keywordInitialize()
+	{
+		keywords = new HashMap();
+		keywords.put("prog", 10);
+		keywords.put("main", 11);
+		keywords.put("fcn", 12);
+		keywords.put("class", 13);
+		keywords.put("float", 15);
+		keywords.put("int", 16);
+		keywords.put("string", 17);
+		keywords.put("if", 18);
+		keywords.put("elseif", 19);
+		keywords.put("else", 20);
+		keywords.put("while", 21);
+		keywords.put("input", 22);
+		keywords.put("print", 23);
+		keywords.put("new", 24);
+		keywords.put("return", 25);
+		keywords.put("var", 26);
+	}
+}
+
+public class Token{
+	int tok;
+	int lin;
+	int linCol;
+	String str;
+	int var;
 
 }
