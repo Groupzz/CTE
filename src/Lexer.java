1c39d0233ae3c25210bb988291eef1eda5110cc6
THEJOSHBOX2000\Josh <joshlorenzen49@gmail.com>
2019-09-22 10:44:17 -0700

    Partial implementation of DFA, needs some work

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,22 +3,38 @@
  * Authors: Aleks Dziewulska, Jamil Khan, Jessica Hilario, Josh Lorenzen
  */
 
+import com.sun.org.apache.xpath.internal.compiler.Keywords;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import static java.lang.System.exit;
 
 public class Lexer {
 
 	static HashMap<String, Integer> keywords;
+	private static HashMap<Character, Integer> tokenLookup;
+	private static ArrayList<Token> tokens = new ArrayList<>();
+
+	private static int state = 1;
+	private static StringBuilder tokenText = new StringBuilder();
+	private static int line = 1;
+	private static int col = 1;
+
 	public static void main(String[] args) throws IOException {
 
 		keywordInitialize();
+		tokenInitialize();
+
 		int ci;
-		int line = 1;
-		int col = 1;
+
 		// Read one character at a time from stdin until EOF
 		while ((ci = System.in.read()) != -1) {
 		    char c = (char) ci;
-			System.out.println(c);
+			//System.out.println(c);
+
+			nextState(c);
 
 			// Increment position counter for each character
 			col++;
@@ -28,6 +44,116 @@ public class Lexer {
 			    line++;
             }
 		}
+
+		for(Token tok : tokens) {
+			System.out.println(tok);
+		}
+	}
+
+	private static void nextState(char c) {
+		if (c == '\n') {
+			if (state == 3) {
+				resetState(); // Throw the comment away
+			}
+			return; // ignore newline
+		}
+		if (c == ' ') {
+			// if we are not reading a string or an identifier
+			//if (state != 21 && state != 13) {
+			if (state == 1) {
+				return; // skip this character
+			}
+		}
+		switch(state) {
+			case 1:
+				if(Character.isAlphabetic(c) || c == '_') {
+					tokenText.append(c);
+					state = 13;
+				}
+				else {
+					switch(c) {
+						case '/':
+							tokenText.append(c);
+							state = 2;
+							break;
+						case '=':
+							tokenText.append(c);
+							state = 5;
+							break;
+						case ',': // token 6
+						case ';': // token 7
+							acceptToken(tokenLookup.get(c), "" + c);
+							resetState();
+							break;
+						case '(':
+
+						default:
+							tokenError();
+							return;
+					}
+				}
+				break;
+			case 2: // Division / line comments
+				if(c == '/'){
+					state = 3;
+					tokenText.append(c);
+				}
+				else {
+					acceptToken(48, tokenText.toString());
+					resetState();
+					nextState(c);
+				}
+				break;
+			case 5:
+				if(c == '=') {
+					tokenText.append(c);
+					state = 6;
+				}
+				else {
+					acceptToken(45, tokenText.toString());
+					resetState();
+					nextState(c);
+				}
+				break;
+			case 13:
+				if(Character.isDigit(c) || Character.isAlphabetic(c) || c == '_') {
+					tokenText.append(c);
+				}
+				else {
+					acceptIdentifier(tokenText.toString());
+					resetState();
+					nextState(c);
+				}
+				break;
+
+		}
+	}
+
+	private static void acceptIdentifier(String text) {
+		Integer keywordID = keywords.get(text);
+		if (keywordID != null) {
+			// its a keyword, use the right tokenID
+			acceptToken(keywordID, text);
+		}
+		else {
+			// its an identifier
+			acceptToken(2, text);
+		}
+	}
+
+	private static void acceptToken(int ID, String text) {
+		Token newToken = new Token(ID, line, col, text);
+		tokens.add(newToken);
+	}
+
+	private static void resetState() {
+		tokenText = new StringBuilder();
+		state = 1;
+	}
+
+	private static void tokenError() {
+		System.out.println("Token syntax error: " + "lin: " + line + " col: " + col);
+		exit(0);
 	}
 
 	static void keywordInitialize()
@@ -49,5 +175,11 @@ public class Lexer {
 		keywords.put("new", 24);
 		keywords.put("return", 25);
 		keywords.put("var", 26);
+	}
+
+	private static void tokenInitialize() {
+		tokenLookup = new HashMap<>();
+		tokenLookup.put(',', 6);
+		tokenLookup.put(';', 7);
 	}
 }
