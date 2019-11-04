af5593a77bff1bf820a87e8ba4be42a265b1fe5d
Jamil Khan <44854522+jamilkhan98@users.noreply.github.com>
2019-11-04 09:54:34 -0800

    Added more symbols up to 21

--- src/Symbol.java
+++ src/Symbol.java
@@ -4,6 +4,26 @@ public class Symbol {
     public static final Symbol MAIN = new Symbol(2, false);
     public static final Symbol BBLOCK = new Symbol(3, false);
 
+    public static final Symbol VARGROUP = new Symbol(4, false);
+    public static final Symbol PPVARLIST = new Symbol(5, false);
+    public static final Symbol VARLIST = new Symbol(6, false);
+    public static final Symbol VARITEM = new Symbol(7, false);
+    public static final Symbol VARDECL = new Symbol(8, false);
+    public static final Symbol SIMPLEKIND = new Symbol(9, false);
+    public static final Symbol BASEKIND = new Symbol(10, false);
+    public static final Symbol CLASSID = new Symbol(11, false);
+    public static final Symbol VARSPEC = new Symbol(12, false);
+    public static final Symbol VARID = new Symbol(13, false);
+    public static final Symbol ARRSPEC = new Symbol(14, false);
+    public static final Symbol KKINT = new Symbol(15, false);
+    public static final Symbol DEREF_ID = new Symbol(16, false);
+    public static final Symbol DEREF = new Symbol(17, false);
+
+    public static final Symbol VARINIT = new Symbol(18, false);
+    public static final Symbol BBEXPRS = new Symbol(19, false);
+    public static final Symbol EXPRLIST = new Symbol(20, false);
+    public static final Symbol MOREEXPRS = new Symbol(21, false);
+    
     private int id;
     private boolean terminal;
     private Token token;
