094506a51db32d3f08f7bbf62cc15419c36f756e
THEJOSHBOX2000\Josh <joshlorenzen49@gmail.com>
2019-11-16 10:41:39 -0800

    reorganize cases

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -227,15 +227,18 @@ public class Parser {
         }
     }
 
-//    private static void pta_hoist1(PNode n){
-//        if(n.getGrandma() != null)
-//        n.hoist(n.getGrandma());
-//        else
-//        System.out.println("hoist1");
-//    }
 
+
+    // This is for non-terminals that have been expanded using an epsilon rule
+    // They have no children and need to disappear
+    // It goes into the parent of the node and nulls out the index pos
     private static void pta_disappear(PNode n, int pos) {
         n.getGrandma().kids[pos] = null;
+    }
+
+    private static void pta_bs1_k1(PNode n){
+        PNode bigSis = n.kids[0];
+        bigSis.hoist(n);
     }
 
     private static void pta_bs1_k2(PNode n){
@@ -278,42 +281,37 @@ public class Parser {
         int rule = n.ruleID;
         switch(rule)
         {
+            // These all fall into eachother on purpose
+            case 15:
+            case 27:
+                pta_bs1_k1(n);
+                break;
             case 1:
             case 2:
             case 4:
             case 6:
             case 12:
+            case 125:
             case 137:
+            case 139:
                 pta_bs1_k2(n);
                 break;
             case 3:
                 pta_bs1_k4(n);
                 break;
-            case 5:
-                pta_disappear(n, 1);
-                break;
             case 7:
                 pta_bs2_k3(n);
                 break;
-            case 8:
-                pta_disappear(n, 2);
+            case 5:
+            case 126:
+                pta_disappear(n, 1);
                 break;
-            case 13:
-            case 14:
-            case 15:
-            case 16: 
-            case 17:
-            case 18:
-            case 21:
-            case 22:
-            case 26:
-            case 27: 
-            case 28:
+            case 8:
             case 66:
                 pta_disappear(n, 2);
                 break;
             default: 
-                System.out.println(n.sym + ": no rule found");
+                System.out.println(n.sym + ": no rule found in AST conversion");
         }
     }
 
